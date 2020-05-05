cb871d41a75d7e579e7169bddcae085fe2b68662
nourahosam <34795230+nourahosam@users.noreply.github.com>
2020-05-05 21:25:27 +0200

    USE THIS

---
+++ SE2Projectt/src/se2projectt/Reservation.java
@@ -0,0 +1,63 @@
+
+package se2projectt;
+
+/**
+ *
+ * @author noura
+ */
+public class Reservation {
+    private int DurationPerNight;
+    private int ReservationID;
+    private String ReservationType;
+    private String ReservationReciept;
+    private Apartment ap;
+
+    public int getDurationPerNight() {
+        return DurationPerNight;
+    }
+
+    public void setDurationPerNight(int DurationPerNight) {
+        this.DurationPerNight = DurationPerNight;
+    }
+
+    public int getReservationID() {
+        return ReservationID;
+    }
+
+    public void setReservationID(int ReservationID) {
+        this.ReservationID = ReservationID;
+    }
+
+    public String getReservationType() {
+        return ReservationType;
+    }
+
+    public void setReservationType(String ReservationType) {
+        this.ReservationType = ReservationType;
+    }
+
+    public String getReservationReciept() {
+        return ReservationReciept;
+    }
+
+    public void setReservationReciept(String ReservationReciept) {
+        this.ReservationReciept = ReservationReciept;
+    }
+    
+    public void TermsandConditions(){
+        
+    }
+    
+    public void Display(){
+        
+    }
+    public void ReservationRequest(){
+        
+    }  
+    public void AddReservation(){
+        
+    }
+    public void CancelReservation(){
+        
+    }
+}
